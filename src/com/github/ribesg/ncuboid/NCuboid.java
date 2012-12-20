111ccfc915e866ba310eb8af25b94537aa8c96f1
Ribesg <Ribesg@Dragon>
2012-12-20 20:10:12 +0100

    Link to Core Interfaces

--- src/com/github/ribesg/ncuboid/NCuboid.java
+++ src/com/github/ribesg/ncuboid/NCuboid.java
@@ -2,6 +2,27 @@ package com.github.ribesg.ncuboid;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class NCuboid extends JavaPlugin {
+import com.github.ribesg.ncore.NCore;
+import com.github.ribesg.ncore.nodes.cuboid.CuboidNode;
+
+public class NCuboid extends JavaPlugin implements CuboidNode {
+
+	@Override
+	public void afterEnable() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setCore(NCore core) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public NCore getCore() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
