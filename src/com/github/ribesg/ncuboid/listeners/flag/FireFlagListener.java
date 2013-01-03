2744843940c63bbd10ae997a794306d6cfa66ab5
Ribesg <Ribesg@yahoo.fr>
2013-01-03 12:53:36 +0100

    Implementation of Fire flag

--- src/com/github/ribesg/ncuboid/listeners/flag/FireFlagListener.java
+++ src/com/github/ribesg/ncuboid/listeners/flag/FireFlagListener.java
@@ -1,6 +1,15 @@
 package com.github.ribesg.ncuboid.listeners.flag;
 
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.block.BlockBurnEvent;
+import org.bukkit.event.block.BlockSpreadEvent;
+
+import com.github.ribesg.ncore.nodes.cuboid.beans.Flag;
 import com.github.ribesg.ncuboid.NCuboid;
+import com.github.ribesg.ncuboid.beans.CuboidDB;
+import com.github.ribesg.ncuboid.beans.PlayerCuboid;
 import com.github.ribesg.ncuboid.listeners.AbstractListener;
 
 public class FireFlagListener extends AbstractListener {
@@ -9,6 +18,21 @@ public class FireFlagListener extends AbstractListener {
         super(instance);
     }
 
-    // TODO
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+    public void onBlockBurn(final BlockBurnEvent event) {
+        final PlayerCuboid cuboid = CuboidDB.getInstance().getPriorByLoc(event.getBlock().getLocation());
+        if (cuboid != null && cuboid.getFlags().get(Flag.FIRE)) {
+            event.setCancelled(true);
+        }
+    }
 
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+    public void onBlockSpread(final BlockSpreadEvent event) {
+        if (event.getBlock().getType() == Material.FIRE) {
+            final PlayerCuboid cuboid = CuboidDB.getInstance().getPriorByLoc(event.getBlock().getLocation());
+            if (cuboid != null && cuboid.getFlags().get(Flag.FIRE)) {
+                event.setCancelled(true);
+            }
+        }
+    }
 }
